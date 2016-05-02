8f8e6f7be49c7d9e02f9cdaaded0e1def61861e1
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2016-05-02 14:21:20 -0700

    initial commit

---
+++ GABeaver.swift
@@ -0,0 +1,77 @@
+//
+//  GABeaver.swift
+//  Journey
+//
+//  Created by Ben Bahrenburg on 5/2/16.
+//  Copyright © 2016 pwc.com. All rights reserved.
+//
+
+import Foundation
+import SwiftyBeaver
+import Google
+
+public class GABeaver: BaseDestination {
+  
+    private let moduleID = 999
+    
+    public var useNSLog = false
+    public var Threshold: SwiftyBeaver.Level  = SwiftyBeaver.Level.Error
+    public var GoogleAnalyticsKey: String?
+    public var printToConsole: Bool = false
+    
+    override public var defaultHashValue: Int {return moduleID}
+   
+    private func createTracker(key: String) {
+        GAI.sharedInstance().trackerWithTrackingId(key)
+        GAI.sharedInstance().logger.logLevel = GAILogLevel.None
+        GAI.sharedInstance().trackUncaughtExceptions = true
+    }
+    
+    private func getTracker() -> GAITracker {
+        return GAI.sharedInstance().defaultTracker
+    }
+
+    private func trackException(reason: String) {
+        let exceptionDictionary = GAIDictionaryBuilder.createExceptionWithDescription(reason, withFatal: false)
+        getTracker().send(exceptionDictionary.build() as [NSObject : AnyObject])
+    }
+    
+    public init(googleAnalyticsKey: String, threshold: SwiftyBeaver.Level  = SwiftyBeaver.Level.Error, printToConsole: Bool = false ) {
+        super.init()
+        self.GoogleAnalyticsKey = googleAnalyticsKey
+        self.Threshold = threshold
+        self.printToConsole = printToConsole
+        self.createTracker(googleAnalyticsKey)
+    }
+    
+    private func performPrint(formattedString: String?) {
+        if let str = formattedString {
+            if useNSLog {
+                NSLog(str)
+            } else {
+                print(str)
+            }
+        }
+    }
+    
+
+    override public func send(level: SwiftyBeaver.Level, msg: String, thread: String,
+                              path: String, function: String, line: Int) -> String? {
+        
+        let formattedString = super.send(level, msg: msg, thread: thread, path: path, function: function, line: line)
+        guard level.rawValue >= self.Threshold.rawValue else {
+            return formattedString
+        }
+        
+        if let str = formattedString {
+            self.trackException(str)
+        }
+        
+        if self.printToConsole {
+            self.performPrint(formattedString)
+        }
+        
+
+        return formattedString
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# GA Beaver - Google Analytics plugin for SwiftyBeaver
+
+
+
+## Author
+
+Ben Bahrenburg, [@bencoding](http://twitter.com/bencoding)
+
+## License
+
+GA Beaver is available under the MIT license. See the LICENSE file for more info.
